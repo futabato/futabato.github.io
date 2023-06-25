b3dfdc8caeaa94dd0b675088c694202146d1a8c7
futabato <01futabato10@gmail.com>
2023-06-26 00:36:26 +0900

    :recycle: nextraの起動がよくわからないのでtemplateから作ることにした

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  unstable_staticImage: true,
+})
+module.exports = withNextra()
